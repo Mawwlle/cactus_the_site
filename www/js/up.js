3ccbbee8ec1f5256dc1af9db34ec002cf9c9f61f
Max <44709362+WindowsME123@users.noreply.github.com>
2019-04-20 16:58:26 +0300

    Delete up.js

--- www/js/up.js
+++
@@ -1,8 +0,0 @@
-var timeOut;
-function scrollToTop() {
-	if (document.body.scrollTop!=10 || document.documentElement.scrollTop!=5){
-		window.scrollBy(0,-50);
-		timeOut=setTimeout('scrollToTop()',10);
-	}
-	else clearTimeout(timeOut);
-}
